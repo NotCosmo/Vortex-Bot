7071d9e34f58e456c82a02e9a4c5f7abd12b2421
Cosmo <85410213+NotCosmo@users.noreply.github.com>
2022-05-08 14:00:24 +0000

    update cogs/utils/weapons.py

--- cogs/utils/weapons.py
+++ cogs/utils/weapons.py
@@ -1,102 +1,92 @@
-BASE_DAMAGES = {
-    "Wooden Sword": (100, 200, 30),
-    "Le sword": (9999, 9999, 100),
-    "Alpha Blade": (300, 400, 45)
+boosts = {"💫": 5, "✨": 15, "⭐": 30, "🌟": 50}
+enhancement_symbols = ["💫", "✨", "⭐", "🌟"]
+base_damage = {
+    "Fists": (40, 80, 5),
+    "Alpha Blade": (100, 160, 20),
+    "OP Weapon": (550, 800, 40),
+    "🔮 OP Weapon": (5500, 8000, 50),
 }
-
-REFORGE_SYMBOLS = ['🌟', '🔮', '⚡']
-REFORGE_NAMES = ["Ethereal", "Valorous", "Fabled"]
 
 class WeaponInstance:
 
-    def __init__(self, display_name: str):
-        self.display = display_name
-        self.get_stats(display_name)
+    def __init__(self, weapon_name: str):
+        self.name = weapon_name
+        self.enhancement = self.get_enhancement()
+        self.boost = self.get_boost()
+        self.clean_name = ""
+        self.min_dmg = 0
+        self.max_dmg = 0
+        self.crit_chance = 0
+        self.get_stats()
 
-    # damage calculations
-    def get_stats(self, display_name: str) -> None:
+    # get enhancement
+    def get_enhancement(self) -> str:
 
-        if display_name[0] in REFORGE_SYMBOLS:
+        if self.name[0] in enhancement_symbols:
+            return self.name[0]
+        return None
+
+    # get boost
+    def get_boost(self) -> int:
+        if self.enhancement:
+            return boosts[self.enhancement]
+        return None
+
+    def get_stats(self) -> None:
+
+        if self.enhancement:
+
             try:
-                self.min_dmg = BASE_DAMAGES[self.strip_reforge(display_name)][0]
-                self.max_dmg = BASE_DAMAGES[self.strip_reforge(display_name)][1]
-                self.crit_chance = BASE_DAMAGES[self.strip_reforge(display_name)][2]
-                self.base_min_dmg = BASE_DAMAGES[self.strip_reforge(display_name)][0]
-                self.base_max_dmg = BASE_DAMAGES[self.strip_reforge(display_name)][1]
-                self.base_crit_chance = BASE_DAMAGES[self.strip_reforge(display_name)][2]
+                self.clean_name = self.name.strip(self.name[0])[1:] # strip enhancement symbol
+                self.base_min_dmg = base_damage[self.clean_name][0]
+                self.base_max_dmg = base_damage[self.clean_name][1]
+                self.base_crit_chance = base_damage[self.clean_name][2]
             except KeyError:
-                self.min_dmg = 100
-                self.max_dmg = 200
-                self.crit_chance = 10
-                self.base_min_dmg = 100
-                self.base_max_dmg = 200
-                self.base_crit_chance = 10
+                self.base_min_dmg = 40
+                self.base_max_dmg = 80
+                self.base_crit_chance = 5
+
+            # if boost is there
+            # add boost to base damage
+            if self.boost:
+                self.min_dmg = int(self.base_min_dmg + (self.boost/100 * self.base_min_dmg))
+                self.max_dmg = int(self.base_max_dmg + (self.boost/100) * self.base_max_dmg)
+                self.crit_chance = int(self.base_crit_chance + (self.boost/100) * self.base_crit_chance)
+
 
         else:
-            try:
-                self.min_dmg = BASE_DAMAGES[display_name][0]
-                self.max_dmg = BASE_DAMAGES[display_name][1]
-                self.crit_chance = BASE_DAMAGES[display_name][2]
-                self.base_min_dmg = BASE_DAMAGES[display_name][0]
-                self.base_max_dmg = BASE_DAMAGES[display_name][1]
-                self.base_crit_chance = BASE_DAMAGES[display_name][2]
-            except KeyError:
-                self.min_dmg = 100
-                self.max_dmg = 200
-                self.crit_chance = 10
-                self.base_min_dmg = 100
-                self.base_max_dmg = 200
-                self.base_crit_chance = 10
+            self.min_dmg = base_damage[self.name][0]
+            self.max_dmg = base_damage[self.name][1]
+            self.crit_chance = base_damage[self.name][2]
 
-        try:
-            reforge = self.get_reforge(display_name)
-            self.min_dmg = reforge.min_dmg
-            self.max_dmg = reforge.max_dmg
-            self.crit_chance = reforge.crit_chance
-            self.name = reforge.name
+    # upgrade weapon and get stats
+    def upgrade(self):
+
+        # if weapon is already at max level
+        if self.enhancement == "🌟":
+            raise Exception("Weapon is already at max level.")
+
+        if self.name[0] == "🔮":
+            raise Exception("Unusuals cannot be upgraded.")
+
+        # weapon does not have enhancement
+        if self.name[0] not in enhancement_symbols:
+            self.enhancement = "💫"
+            self.name = f"{self.enhancement} {self.name}"
+            self.boost = boosts[self.enhancement]
+            self.get_stats()
             return self
 
-        # No reforge found
-        except AttributeError:
-            self.name = display_name
+        # weapon has tiers 1-3
+        else:
+            self.new_enhancement = enhancement_symbols[enhancement_symbols.index(self.enhancement) + 1]
+            self.name = self.name.replace(self.enhancement, self.new_enhancement)
+            self.enhancement = self.new_enhancement
+            self.boost = boosts[self.enhancement]
+            self.get_stats()
             return self
 
-    # Strip reforge symbol from name
-    def strip_reforge(self, display_name: str) -> None:
-        char = display_name[0]
-        return display_name.strip(char)[1:]
-
-    # Returns reforge name, and damages
-    def get_reforge(self, display_name: str) -> None:
-
-        """
-        ✦ - Celestial (More Damage)
-        """
-
-        char = display_name[0]
-        # ethereal reforge
-        if char == '🌟':
-            self.min_dmg += int((30/100) * self.min_dmg)
-            self.max_dmg += int((30/100) * self.max_dmg)
-            self.crit_chance += int((15/100) * self.crit_chance)
-            self.name = f"{char} Ethereal {self.strip_reforge(display_name)}"
-            return self
-
-        elif char == "🔮":
-            self.min_dmg += int((5/100) * self.min_dmg)
-            self.max_dmg += int((5/100) * self.max_dmg)
-            self.crit_chance += int((5/100) * self.crit_chance)
-            self.name = f"{char} Fabled {self.strip_reforge(display_name)}"
-            return self
-
-        # VALOROUS REFORGE
-        elif char == "⚡":
-            self.min_dmg += int((10/100) * self.min_dmg)
-            self.max_dmg += int((10/100) * self.max_dmg)
-            self.crit_chance += int((30/100) * self.crit_chance)
-            self.name = f"{char} Valorous {self.strip_reforge(display_name)}"
-            return self
-
-    @property
-    def strip_name(self) -> str:
-        return self.display.strip('⚡🔮🌟')[1:]
+    def __str__(self) -> str:
+        if self.boost:
+            return f"Min: {self.min_dmg} (+{self.boost}%)\nMax: {self.max_dmg} (+{self.boost}%)\nCrit: {self.crit_chance}% (+{self.boost}%)"
+        return f"Min: {self.min_dmg}\nMax: {self.max_dmg}\nCrit: {self.crit_chance}%"
